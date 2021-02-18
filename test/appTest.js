87a94f68c8d6d30110b2032fd15c9e325ca279da
littleflute <littleflute@163.com>
2021-02-17 16:28:19 -0800

    test/appTest.js*

--- test/appTest.js
+++ test/appTest.js
@@ -2,7 +2,7 @@ const assert = require('chai').assert;
 const app = require('../app');
 
 describe('App', function(){
-  it('app should return hello', function(){
-    assert.equal(app(),'hello');
+  it('app should return Hello', function(){
+    assert.equal(app(),'Hello');
   });
 });
