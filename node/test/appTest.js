521223618d1a0f446c116d55c44ac53945ff4809
littleflute <littleflute@163.com>
2021-02-17 17:34:12 -0800

    appTest.js*

--- node/test/appTest.js
+++ node/test/appTest.js
@@ -3,6 +3,6 @@ const app = require('../app');
 
 describe('App', function(){
   it('app should return Hello', function(){
-    assert.equal(app(),'Hello ci test1.');
+    assert.equal(app(),'Hello ci test:1.');
   });
 });
